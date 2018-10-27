e9fca57c98d1cfc0dc807bbe3a92555ea55ca18e
Ragnarrlothbrok <Ragnarrlothbrok@gmail.com>
2018-10-27 17:37:32 +0530

    asd

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require("body-parser");
+
 
 var indexRouter = require('./routes/index');
+
+//new
+var Pusher = require("pusher");
+
+var pusher = new Pusher({
+  appId: "618134",
+  key: "d99dca5d5ba5eddba9a2",
+  secret: "a36aaf13c2e3004012a4",
+  cluster: "ap2",
+  encrypted: true
+});
+//
 
 var app = express();
 
@@ -12,6 +26,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +35,62 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+
+//new2
+var londonTempData = {
+  city: "London",
+  unit: "celsius",
+  dataPoints: [
+    {
+      time: 1130,
+      temperature: 12
+    },
+    {
+      time: 1200,
+      temperature: 13
+    },
+    {
+      time: 1230,
+      temperature: 15
+    },
+    {
+      time: 1300,
+      temperature: 14
+    },
+    {
+      time: 1330,
+      temperature: 15
+    }
+  ]
+};
+
+app.get("/getTemperature", function(req, res) {
+  res.send(londonTempData);
+});
+
+app.get("/addTemperature", function(req, res) {
+  var temp = parseInt(req.query.temperature);
+  var time = parseInt(req.query.time);
+  if (temp && time && !isNaN(temp) && !isNaN(time)) {
+    var newDataPoint = {
+      temperature: temp,
+      time: time
+    };
+    londonTempData.dataPoints.push(newDataPoint);
+    pusher.trigger("london-temp-chart", "new-temperature", {
+      dataPoint: newDataPoint
+    });
+    res.send({ success: true });
+  } else {
+    res.send({
+      success: false,
+      errorMessage: "Invalid Query Paramaters, required - temperature & time."
+    });
+  }
+});
+//2end
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- temp.js
+++ temp.js
@@ -1,49 +1,105 @@
-const ctx= document.getElementsByClassName("chart");
-//             let data = [{
-//         x: 10,
-//         y: 20
-//     }, {
-//         x: 15,
-//         y: 10
-//     }];
-//     var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         //labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [{
-//             label: '# of Votes',
-//             data:data ,
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// });
-
-// //     let myLineChart = new Chart(ctx, {
+// const ctx= document.getElementsByClassName("chart");
+// //             let data = [{
+// //         x: 10,
+// //         y: 20
+// //     }, {
+// //         x: 15,
+// //         y: 10
+// //     }];
+// //     var myChart = new Chart(ctx, {
 // //     type: 'line',
-// //     data: data
+// //     data: {
+// //         //labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+// //         datasets: [{
+// //             label: '# of Votes',
+// //             data:data ,
+// //             backgroundColor: [
+// //                 'rgba(255, 99, 132, 0.2)',
+// //                 'rgba(54, 162, 235, 0.2)',
+// //                 'rgba(255, 206, 86, 0.2)',
+// //                 'rgba(75, 192, 192, 0.2)',
+// //                 'rgba(153, 102, 255, 0.2)',
+// //                 'rgba(255, 159, 64, 0.2)'
+// //             ],
+// //             borderColor: [
+// //                 'rgba(255,99,132,1)',
+// //                 'rgba(54, 162, 235, 1)',
+// //                 'rgba(255, 206, 86, 1)',
+// //                 'rgba(75, 192, 192, 1)',
+// //                 'rgba(153, 102, 255, 1)',
+// //                 'rgba(255, 159, 64, 1)'
+// //             ],
+// //             borderWidth: 1
+// //         }]
+// //     },
+// //     options: {
+// //         scales: {
+// //             yAxes: [{
+// //                 ticks: {
+// //                     beginAtZero:true
+// //                 }
+// //             }]
+// //         }
+// //     }
 // // });
+
+// // //     let myLineChart = new Chart(ctx, {
+// // //     type: 'line',
+// // //     data: data
+// // // });
+
+// body {
+//     margin: 0;
+//     padding: 0;
+//     overflow: hidden;
+//     font-family: Raleway;
+//   }
+  
+//   .loader,
+//   .loader:after {
+//     border-radius: 50%;
+//     width: 10em;
+//     height: 10em;
+//   }
+//   .loader {
+//     margin: 60px auto;
+//     font-size: 10px;
+//     position: relative;
+//     text-indent: -9999em;
+//     border-top: 1.1em solid rgba(82, 0, 115, 0.2);
+//     border-right: 1.1em solid rgba(82, 0, 115, 0.2);
+//     border-bottom: 1.1em solid rgba(82, 0, 115, 0.2);
+//     border-left: 1.1em solid #520073;
+//     -webkit-transform: translateZ(0);
+//     -ms-transform: translateZ(0);
+//     transform: translateZ(0);
+//     -webkit-animation: load8 1.1s infinite linear;
+//     animation: load8 1.1s infinite linear;
+//   }
+//   @-webkit-keyframes load8 {
+//     0% {
+//       -webkit-transform: rotate(0deg);
+//       transform: rotate(0deg);
+//     }
+//     100% {
+//       -webkit-transform: rotate(360deg);
+//       transform: rotate(360deg);
+//     }
+//   }
+//   @keyframes load8 {
+//     0% {
+//       -webkit-transform: rotate(0deg);
+//       transform: rotate(0deg);
+//     }
+//     100% {
+//       -webkit-transform: rotate(360deg);
+//       transform: rotate(360deg);
+//     }
+//   }
+  
+//   #weatherChart {
+//     height: 500px !important;
+//     width: 600px !important;
+//     margin: 0 auto;
+//   }
+  
